d070e8458b6f5613c1b0ce236107440ab428de3e
Pepper <jenearly@gmail.com>
2019-11-18 19:47:01 -0500

    get rid of sw until i figure out why there is a problem

--- src/index.js
+++ src/index.js
@@ -5,17 +5,17 @@ require('./favicon.ico');
 
 
 //add serviceWorker 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function () {
-    navigator.serviceWorker.register('./sw.js').then(function (registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function (err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', function () {
+//     navigator.serviceWorker.register('./sw.js').then(function (registration) {
+//       // Registration was successful
+//       console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//     }, function (err) {
+//       // registration failed :(
+//       console.log('ServiceWorker registration failed: ', err);
+//     });
+//   });
+// }
 
 //reload on changes
 if (isDev) {
@@ -30,10 +30,10 @@ require('./component/index/react.jsx');
 require('./index.html');
 
 //for heroku 
-if (isProd) {
-  var http = require('http');
-  setInterval(function() {
-    http.get('https://talking-cloud.herokuapp.com/');
-  }, 300000); // every 5 minutes (300000)
-}
+// if (isProd) {
+//   var http = require('http');
+//   setInterval(function() {
+//     http.get('https://talking-cloud.herokuapp.com/');
+//   }, 300000); // every 5 minutes (300000)
+// }
 console.log('Environment: ' + process.env.NODE_ENV);
