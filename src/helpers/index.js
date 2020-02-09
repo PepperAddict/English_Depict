57202b697e29111bd1a36da6acde16e0273b3ee6
unknown <jenearly@gmail.com>
2020-02-09 17:40:46 -0500

    test

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,6 +1,7 @@
 const Cryptr = require('cryptr');
 const jwt = require('jsonwebtoken');
-const cryptr = new Cryptr(process.env.SALT);
+const okaythis = "nonzerostring"
+const cryptr = new Cryptr(okaythis);
 
 export const clearCookies = (keyName = null) => {
   let expireDate = new Date();
