b25f46de9b611b6345a6262cbee516fe8739f4d4
unknown <jenearly@gmail.com>
2020-02-18 12:33:25 -0500

    hide multer for now

--- server/index.js
+++ server/index.js
@@ -92,54 +92,54 @@ server.use('/graphql', cors(), (req, res) => {
 
 //route for images
 
-const multer = require('multer');
-const multerAzure = require('multer-azure')
+// const multer = require('multer');
+// const multerAzure = require('multer-azure')
 
 
-const upload = multer({
-  storage: multerAzure({
-    connectionString: process.env.AZURE_STORAGE_STRING,
-    account: 'talkingstorage',
-    key: process.env.AZURE_STORAGE_KEY,
-    container: 'talkingcontainer',
-    blobPathResolver: function(req, file, callback){
-      var blobPath = path.basename(Date.now().toString() + path.extname(file.originalname))
-      callback(null, blobPath);
-    }
+// const upload = multer({
+//   storage: multerAzure({
+//     connectionString: process.env.AZURE_STORAGE_STRING,
+//     account: 'talkingstorage',
+//     key: process.env.AZURE_STORAGE_KEY,
+//     container: 'talkingcontainer',
+//     blobPathResolver: function(req, file, callback){
+//       var blobPath = path.basename(Date.now().toString() + path.extname(file.originalname))
+//       callback(null, blobPath);
+//     }
 
-  }),
-  fileFilter: function(req, file, cb) {
-    const filetypes = /jpeg|jpg|png|svg|gif/;
-    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-    const mimetype = filetypes.test(file.mimetype);
-    if (mimetype && extname) {
-      return cb(null, true);
-    } else {
-      cb(null, false);
-    }
-  }
-})
+//   }),
+//   fileFilter: function(req, file, cb) {
+//     const filetypes = /jpeg|jpg|png|svg|gif/;
+//     const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+//     const mimetype = filetypes.test(file.mimetype);
+//     if (mimetype && extname) {
+//       return cb(null, true);
+//     } else {
+//       cb(null, false);
+//     }
+//   }
+// })
 
 
-server.post('/upload', upload.single('depictImage'), (req, res, next) => {
-  if (req.file === undefined) {
-    return res.status(400).json({
-      msg: 'Image only'
-    });
-  }else if(req.file === null) {
-    return res.status(400).json({
-      msg: 'No file was uploaded'
-    });
-  } else {
+// server.post('/upload', upload.single('depictImage'), (req, res, next) => {
+//   if (req.file === undefined) {
+//     return res.status(400).json({
+//       msg: 'Image only'
+//     });
+//   }else if(req.file === null) {
+//     return res.status(400).json({
+//       msg: 'No file was uploaded'
+//     });
+//   } else {
 
-    const imageName = req.file.bobPath; 
-    const imageLocation = req.file.url; 
-    res.json( {
-      image: imageName, 
-      location: imageLocation
-    });
-  }
-});
+//     const imageName = req.file.bobPath; 
+//     const imageLocation = req.file.url; 
+//     res.json( {
+//       image: imageName, 
+//       location: imageLocation
+//     });
+//   }
+// });
 
 
 
