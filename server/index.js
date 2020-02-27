f5d8b01e86fc9c01cd87859805e58eee6aa3d375
unknown <jenearly@gmail.com>
2020-02-26 19:43:04 -0500

    add port

--- server/index.js
+++ server/index.js
@@ -43,7 +43,8 @@ const pgPool = new pg.Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB
+  database: process.env.DB,
+  port: process.env.DB_PORT
 });
 const schema = require('./schema/');
 
