7767d69bdc8da1855adfde649c9b56c78dfe9703
unknown <jenearly@gmail.com>
2020-04-02 23:52:35 -0400

    update student

--- src/component/Student/StudentTasks.tsx
+++ src/component/Student/StudentTasks.tsx
@@ -1,12 +1,14 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState, Fragment, useEffect } from 'react';
 
 function CIC(props) {
-    const [tasks] = useState(props.task)
+    const [tasks] = useState(props.task);
+
+
     return (
     <div className="task-CIC">
-        <a href={`/todo/task?=${tasks.task_id}`}>
-            <img src={tasks.entry.clue_image.urls.thumb} alt={tasks.entry.clue_image.alt_description} />
-        </a>
+        {/* <a href={`/todo/task?=${tasks.task_id}`}> */}
+            <img src={tasks.entry.clue_image.urls.thumb} alt={tasks.entry.clue_image.alt_description} onClick={ e => props.setSelectedTask(tasks)} />
+        {/* </a> */}
     </div>
     )
 }
@@ -14,14 +16,19 @@ function CIC(props) {
 //Only show the tasks that are not accepted
 
 function Task(props) {
-    const [tasks] = useState(props.content)
+    const [tasks] = useState(props.content);
+    const [selectedTask, setSelectedTask] = useState(null)
+
+    if (selectedTask) {
+        console.log(selectedTask)
+    }
 
     return (
         <Fragment>
             {!tasks.accepted ? 
             <div className="task-container">
                 {tasks.task_code === "CIC" &&
-                <CIC task={tasks}/>}
+                <CIC task={tasks} setSelectedTask={setSelectedTask}/>}
             </div>
             : null}
         </Fragment>
