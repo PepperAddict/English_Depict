515e9df331c64bfce19566c3fcf50125627739b2
unknown <jenearly@gmail.com>
2020-04-10 21:36:14 -0400

    send over task from clicking student name instead

--- src/component/Teacher/ShowTaskIndividual.tsx
+++ src/component/Teacher/ShowTaskIndividual.tsx
@@ -9,7 +9,7 @@ return (
     <Doughnut chart={props.data.forChart} use={props.data.id}/>
     <span>students: {
         props.each.map((x, key) => {
-            return <p key={key}>{x.student_name} : {(!x.submission) ? "Not Completed Yet" : x.submission + ' | waiting for review'}</p>
+            return <p key={key} onClick={e => props.setShowTask(x)}>{x.student_name} : {(!x.submission) ? "Not Completed Yet" : x.submission + ' | waiting for review'}</p>
         })
     }</span>
 </Fragment>
@@ -24,15 +24,15 @@ export default function IndividualTask(props) {
         <Fragment>
             {filteredTask.map((ft, key) => {
                 if (ft.task === "WOTD" && ft.forChart.finished !== ft.forChart.inall && props.type === "WOTD") {
-                    return <div key={key} onClick={e => props.setShowTask(ft.tasks[0])}>
-                        <TaskStudentList each={ft.tasks} data={ft}/>
+                    return <div key={key} >
+                        <TaskStudentList each={ft.tasks} data={ft} setShowTask={props.setShowTask}/>
                         
                         <p>word: {ft.id}</p>
                         <p>Example Sentence: {ft.tasks[0].entry.sentence}</p>
                     </div>
                 } else if (ft.task === "CIC" && ft.forChart.finished !== ft.forChart.inall && props.type === "CIC") {
                     return <div key={key} onClick={e => props.setShowTask(ft.tasks[0])}>
-                  <TaskStudentList each={ft.tasks} data={ft}/>
+                  <TaskStudentList each={ft.tasks} data={ft} setShowTask={props.setShowTask}/>
  
                   <img src={ft.tasks[0].entry.clue_image.urls.thumb} alt={ft.tasks[0].entry.clue_image.alt_description} />
                 </div>
