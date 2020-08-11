4318aa62f3f127e03e439335f708e6e62899e37c
unknown <jenearly@gmail.com>
2020-08-11 17:33:40 -0400

    does it need proxy

--- server/middleware/puppet.js
+++ server/middleware/puppet.js
@@ -6,20 +6,26 @@ const cors = require('cors')
 const router = express.Router();
 const path = require("path");
 const puppeteer = require('puppeteer');
-const { response } = require("express");
+const useProxy = require('puppeteer-page-proxy')
 
 
-router.get(['/api/1/puppeteer/', '/api/1/puppeteer/:page?'], async (req, res) => {
+router.get(['/api/1/puppeteer/', '/api/1/puppeteer/:page?'], cors(), async (req, res) => {
+    console.log(req.query.url)
 
     try {
         //allow all origin and limit when there are problems
         res.header('Access-Control-Allow-Origin', '*');        
         const browser = await puppeteer.launch({
             headless: true,
-            args: ['--no-sandbox']
+            args: ['--no-sandbox', '--disable-web-security']
         });
+        
         const page = await browser.newPage();
-        await page.goto(req.query.url, {waitUntil: 'networkidle0'});
+        await page.setRequestInterception(true);
+        page.on('request', async request => {
+            await useProxy(request, 'https://127.0.0.1:8080');
+        });
+        await page.goto(req.query.url);
         const image = await page.screenshot({type: "jpeg", quality: 50});
         await browser.close();
         res.set('Content-Type', 'image/jpeg');
