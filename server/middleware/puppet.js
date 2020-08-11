5de829b1853f22c9a9b3d4eb8621ef6b6f414b52
unknown <jenearly@gmail.com>
2020-08-11 19:33:35 -0400

    trying different proxy package

--- server/middleware/puppet.js
+++ server/middleware/puppet.js
@@ -1,34 +1,42 @@
-
-
 const express = require("express");
 require("dotenv").config();
-const cors = require('cors')
+const cors = require("cors");
 const router = express.Router();
 const path = require("path");
-const puppeteer = require('puppeteer');
-const useProxy = require('puppeteer-page-proxy')
+const puppeteer = require("puppeteer");
+const {
+    proxyRequest,
+  } =require('puppeteer-proxy');
 
-
-router.get(['/api/1/puppeteer/', '/api/1/puppeteer/:page?'], cors(), async (req, res) => {
-    console.log(req.query.url)
-
-    try {
-        //allow all origin and limit when there are problems
-        res.header('Access-Control-Allow-Origin', '*');        
-        const browser = await puppeteer.launch({
-            args: ['--no-sandbox', '--disable-web-security', '--proxy-server=127.0.0.1:8080']
+router.get(
+  ["/api/1/puppeteer/", "/api/1/puppeteer/:page?"],
+  cors(),
+  async (req, res) => {
+    //allow all origin and limit when there are problems
+    res.header("Access-Control-Allow-Origin", "*");
+    const browser = await puppeteer.launch({
+      args: ["--disable-web-security"],
+    });
+    const page = await browser.newPage();
+    await page.setRequestInterception(true);
+    page.on('request', async (request) => {
+        await proxyRequest({
+          page,
+          proxyUrl: 'http://127.0.0.1:8080',
+          request,
         });
-        
-        const page = await browser.newPage();
-        page.setExtraHTTPHeaders({proxy_addr: "127.0.0.1", proxy_port: '8080'});
-        await page.goto(req.query.url);
-        const image = await page.screenshot({type: "jpeg", quality: 50});
-        await browser.close();
-        res.set('Content-Type', 'image/jpeg');
-        res.send(image);
+      });
+    try {
+
+      await page.goto(req.query.url, {waitUntil: 'load'});
+      const image = await page.screenshot({ type: "jpeg", quality: 50 });
+      await browser.close();
+      res.set("Content-Type", "image/jpeg");
+      res.send(image);
     } catch (error) {
-        console.log(error);
+      console.log(error);
     }
-})
+  }
+);
 
 module.exports = router;
