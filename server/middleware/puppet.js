d617718f46ca01adb5ec374148459dbb5a3176df
unknown <jenearly@gmail.com>
2020-08-11 18:57:38 -0400

    test if this works

--- server/middleware/puppet.js
+++ server/middleware/puppet.js
@@ -16,14 +16,11 @@ router.get(['/api/1/puppeteer/', '/api/1/puppeteer/:page?'], cors(), async (req,
         //allow all origin and limit when there are problems
         res.header('Access-Control-Allow-Origin', '*');        
         const browser = await puppeteer.launch({
-            args: ['--no-sandbox', '--disable-web-security']
+            args: ['--no-sandbox', '--disable-web-security', '--proxy-server=127.0.0.1:8080']
         });
         
         const page = await browser.newPage();
-        await page.setRequestInterception(true);
-        page.on('request', async request => {
-            await useProxy(request, 'http://127.0.0.1:8080/');
-        });
+        page.setExtraHTTPHeaders({proxy_addr: "127.0.0.1", proxy_port: '8080'});
         await page.goto(req.query.url);
         const image = await page.screenshot({type: "jpeg", quality: 50});
         await browser.close();
