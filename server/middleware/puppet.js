67bd06f60b94fbad8d2639a64d42b95ebe35a0bc
unknown <jenearly@gmail.com>
2020-08-11 16:55:58 -0400

    enable path

--- server/middleware/puppet.js
+++ server/middleware/puppet.js
@@ -9,15 +9,16 @@ const puppeteer = require('puppeteer');
 const { response } = require("express");
 
 
-router.get(['/api/1/puppeteer/'], async (req, res) => {
+router.get(['/api/1/puppeteer/', '/api/1/puppeteer/:page?'], async (req, res) => {
     try {
         //allow all origin and limit when there are problems
         res.header('Access-Control-Allow-Origin', '*');        
         const browser = await puppeteer.launch({
+            headless: true,
             args: ['--no-sandbox']
         });
         const page = await browser.newPage();
-        await page.goto(req.query.url);
+        await page.goto(req.query.url, {waitUntil: 'networkidle0'});
         const image = await page.screenshot({type: "jpeg", quality: 50});
         await browser.close();
         res.set('Content-Type', 'image/jpeg');
