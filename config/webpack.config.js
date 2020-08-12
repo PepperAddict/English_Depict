bbd4dcfcdce18e7b864487e867148d93ff32da0b
unknown <jenearly@gmail.com>
2020-08-11 21:25:19 -0400

    fixing some audit

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -221,15 +221,19 @@ module.exports = {
         filename: path.resolve(__dirname, '../dist/index.html'),
         awsClient: process.env.AMACLIENT
       }),
-      new CopyPlugin( [
+      new CopyPlugin( {
+                patterns: [
         //send service worker and manifest over to dist folder
         // { from: path.resolve(__dirname, '../src/sw.js'), to: path.resolve(__dirname, '../dist/sw.js')},
-        { from: path.resolve(__dirname, '../src/manifest.json'), to: path.resolve(__dirname, '../dist/manifest.json')}
-      ])
+        { 
+        from: path.resolve(__dirname, '../src/manifest.json'), 
+        to: path.resolve(__dirname, '../dist/manifest.json')
+      }
+      ]
+      }
+)
       // new CleanWebpackPlugin(),
-      // new CopyPlugin([
-      //   { from: '../components/images', to: 'images'}
-      // ])
+
     );
 
     if (isDev) {
