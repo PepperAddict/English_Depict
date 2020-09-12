56b264e14684f041cf818cb0c74807815ecf65cd
unknown <jenearly@gmail.com>
2020-09-12 00:05:51 -0400

    fix for math

--- server/middleware/math.js
+++ server/middleware/math.js
@@ -11,7 +11,7 @@ function mathy(res, mode, callback) {
   const modeLevel = mode == "hard" ? false : true;
   //Get new math problems once a day
 
-  schedule.scheduleJob("*/1 * * * *", (fireDate) => {
+  schedule.scheduleJob("*/60 * * * *", (fireDate) => {
       sayProb = {
           easy : sayProblem(true),
           hard : sayProblem(false)
@@ -32,7 +32,6 @@ router.get(["/api/1/math/cashier/:page?"], (req, res) => {
 router.get("/api/2/math", (req, res) => {
   res.setHeader("Content-Type", "application/json");
   mathy(res, "easy", (data) => {
-    console.log(data);
     return res.send(data);
   });
 });
